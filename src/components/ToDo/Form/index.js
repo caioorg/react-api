c9a3b36892ecac7de81e70b15c9c0e3298957df6
Caio Almeida <caio.almeida@justdigital.com.br>
2019-04-01 19:53:18 -0300

    Added request service reducers and actions

--- src/components/ToDo/Form/index.js
+++ src/components/ToDo/Form/index.js
@@ -1,34 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Grid from '../../Utility/gridSetting'
 import ButtonAction from '../../Utility/buttonAction'
 import './index.scss'
 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { changeDescription } from '../../../store/actions/toDoAction'
+import { changeDescription, searchToDO } from '../../../store/actions/toDoAction'
 
-const FormToDo = props => (
-    <div role='form' className='toDoForm'>
-        <div className="row">
-            <Grid cols="8 8 8 8 8">
-                <input 
-                    id="description"
-                    className="form-control"
-                    placeholder="Adicione uma tarefa"
-                    value={props.description}
-                    onChange={props.changeDescription}
-                />
-            </Grid>
-            <Grid cols="4 4 4 4 4">
-                <ButtonAction style="success" onClick={props.handleAdd} text="Adicionar" />
-                <ButtonAction style="primary" onClick={props.handleSearch} text="Pesquisar" />
-                <ButtonAction style="secondary" onClick={props.handleClear} text="Limpar" />
-            </Grid>
-        </div>
-    </div>
-)
+class FormToDo extends Component {
+    constructor(props) {
+        super(props)
+        this.keyHandler = this.keyHandler.bind(this)
+    }
+
+    keyHandler(e) {
+        if (e.key === 'Enter') {
+            e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
+        } else if (e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
+
+    componentWillMount() {
+        this.props.searchToDO()
+    }
+
+    render() {
+        return (
+            <div role='form' className='toDoForm'>
+                <div className="row">
+                    <Grid cols="8 8 8 8 8">
+                        <input 
+                            id="description"
+                            className="form-control"
+                            placeholder="Adicione uma tarefa"
+                            value={this.props.description}
+                            onChange={this.props.changeDescription}
+                        />
+                    </Grid>
+                    <Grid cols="4 4 4 4 4">
+                        <ButtonAction style="success" onClick={this.props.handleAdd} text="Adicionar" />
+                        <ButtonAction style="primary" onClick={this.props.handleSearch} text="Pesquisar" />
+                        <ButtonAction style="secondary" onClick={this.props.handleClear} text="Limpar" />
+                    </Grid>
+                </div>
+            </div>
+        )
+    }
+}
+
 
 const mapStateToProps = state => ({ description: state.todo.description })
-const mapDispatchToProps = dispatch => bindActionCreators({ changeDescription }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ changeDescription, searchToDO }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(FormToDo)
--- src/store/actions/toDoAction.js
+++ src/store/actions/toDoAction.js
@@ -1,4 +1,14 @@
+import services from '../../services'
+
 export const changeDescription = event => ({
     type: 'DESCRIPTION_CHANGED',
     payload: event.target.value
 })
+
+export const searchToDO = () => {
+    const request = services.listToDo()
+    return {
+        type: 'TODO_SEARCHED',
+        payload: request
+    }
+}
